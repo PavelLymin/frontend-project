5810836b1aeff845fcc4d1942b48785a1071900a
pavel <pavel.lyamin2005@mail.ru>
2025-09-17 12:22:34 +0700

    The website header is done

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
 export default {
   plugins: {
-    autoprefixer: {},
-    'postcss-nesting': {},
-    'postcss-custom-media': {},
-    '@csstools/postcss-media-minmax': {},
+    'postcss-custom-media': {
+      importFrom: [
+        {
+          customMedia: {
+            '--sm-viewport': '(min-width: 576px)',
+            '--md-viewport': '(min-width: 768px)',
+            '--lg-viewport': '(min-width: 1024px)',
+            '--xl-viewport': '(min-width: 1280px)',
+          },
+        },
+      ],
+    },
   },
 };
