66a0b2dfaff1acd73e6beb71100bbe4f56127d15
pavel <pavel.lyamin2005@mail.ru>
2025-10-19 16:49:49 +0700

    Made all the sections of the site

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './index.css';
+document.addEventListener('DOMContentLoaded', function () {
+  const itemTitles = document.querySelectorAll('.features__item-heading');
+
+  itemTitles.forEach((title) => {
+    title.addEventListener('click', () => {
+      const parentItem = title.closest('.features__item');
+      parentItem.classList.toggle('features__item--open');
+    });
+  });
+});
