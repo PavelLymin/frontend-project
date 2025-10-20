ad801400beb4457d2a92588047036374b4f2afb6
pavel <pavel.lyamin2005@mail.ru>
2025-10-20 23:56:15 +0700

    Added adaptive

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,8 @@
 export default {
   plugins: {
-    'postcss-custom-media': {
-      importFrom: [
-        {
-          customMedia: {
-            '--sm-viewport': '(min-width: 576px)',
-            '--md-viewport': '(min-width: 768px)',
-            '--lg-viewport': '(min-width: 1024px)',
-            '--xl-viewport': '(min-width: 1280px)',
-          },
-        },
-      ],
-    },
+    autoprefixer: {},
+    'postcss-nesting': {},
+    'postcss-custom-media': {},
+    '@csstools/postcss-media-minmax': {},
   },
 };
